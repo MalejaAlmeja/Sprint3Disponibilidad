ab5a4a09262f86d08c7a02b8e222661fd6107085
Ma Alejandra Carrillo <ma.carrillo2@uniandes.edu.co>
2025-11-03 08:08:49 -0500

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -334,20 +334,3 @@ resource "aws_cloudwatch_metric_alarm" "latency_alarm" {
   threshold           = 1000
   comparison_operator = "GreaterThanThreshold"
 }
-
-# --------------------------------------------- Outputs ---------------------------------------------
-output "db_endpoints" {
-  value = {
-    db1 = aws_db_instance.db1.address
-    db2 = aws_db_instance.db2.address
-    db3 = aws_db_instance.db3.address
-  }
-}
-
-output "lambda_invoke_arn" {
-  value = aws_lambda_function.consistency.arn
-}
-
-output "event_rule_name" {
-  value = aws_cloudwatch_event_rule.detect_inconsistency.name
-}
